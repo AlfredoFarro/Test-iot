e373f7bb0a3f3b1a3bc1fd084e023b440aa6680d
AlfredoFarro <u201911924@upc.edu.pe>
2024-05-30 02:18:05 -0500

    Test

---
+++ src/main.js
@@ -0,0 +1,52 @@
+import './assets/main.css'
+import 'primevue/resources/themes/lara-light-indigo/theme.css'
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css'
+import { createApp } from 'vue'
+import { createRouter,createWebHistory } from 'vue-router'
+import App from './App.vue'
+import Login from './views/login.vue'
+import Products from './views/products.vue'
+import InfoProducts from './views/info-products.vue'
+import Register from './views/register.vue'
+import Payment_methods from './views/payment-methods.vue'
+import Payment_confirm from './views/payment-confirm.vue'
+import User_profile from './views/user-profile.vue'
+import Page_not_found from './views/page-not-found.vue'
+
+
+
+import PrimeVue from 'primevue/config';
+import SelectButton from 'primevue/selectbutton';
+import Button from 'primevue/button';
+import Card from 'primevue/card';
+import InputText from 'primevue/inputtext';
+import FloatLabel from 'primevue/floatlabel';
+import Dropdown from 'primevue/dropdown';
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {path: '/', name: '', component:Products},
+        {path: '/login', name: 'Login', component:Login},
+        {path: '/products', name: 'Products', component:Products},
+        {path: '/infoproducts', name: 'InfoProducts', component:InfoProducts},
+        {path: '/register', name: 'Register', component:Register},
+        {path: '/payment-methods', name: 'Payment-methods', component:Payment_methods},
+        {path: '/payment-confirm', name: 'Payment-confirm', component:Payment_confirm},
+        {path: '/user-profile', name: 'User-profile', component:User_profile},
+        {path: '/:pathMatch(.*)*', name: 'NotFound', component: Page_not_found}
+        
+    ]
+})
+createApp(App)
+.use(router)
+.use(PrimeVue)
+
+.component('SelectButton',SelectButton)
+.component('Button',Button)
+.component('Card',Card)
+.component('InputText',InputText)
+.component('FloatLabel',FloatLabel)
+.component('Dropdown',Dropdown)
+.mount('#app')
---
+++ src/views/register.vue
@@ -0,0 +1,81 @@
+<template>
+    <div class="pagina">
+        <div class="img-container">
+            <img alt="user header" src="../assets/logo_lila.png"/>
+        </div>
+        <div class="sign-in-container">
+            <div class="title">
+                <h1 class="bolder">Sign In</h1>
+            </div>
+            <div class="flex flex-column gap-2">
+                <FloatLabel style="margin-top: 1rem;">
+                    <InputText style="width: 350px;border-radius: 100px;" id="username" v-model="value" />
+                    <label for="username" class="normal">Enter Email</label>
+                </FloatLabel>
+                <FloatLabel style="margin-top: 1rem;">
+                    <InputText style="width: 350px;border-radius: 100px;" id="username" v-model="value" />
+                    <label for="username" class="normal">Enter Name</label>
+                </FloatLabel>
+                <FloatLabel style="margin-top: 1rem;">
+                    <InputText style="width: 350px;border-radius: 100px;" id="username" v-model="value" />
+                    <label for="username" class="normal">Enter Password</label>
+                </FloatLabel>
+            </div>
+            <div class="buttons">
+                <Button style="margin-top: 1rem;width: 150px;border-radius: 100px;text-align: center;" class="buy-button"label="Log In" />
+                <Button style="margin-top: 1rem;border-radius: 100px;text-align: center;" class="buy-button"label="Create Account" />
+                
+            </div>
+        </div>
+        
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+    
+    .pagina{
+        height: 100vh;
+        width: 100vw;
+        background-color: var(--indigo-500);
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+    .sign-in-container{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        height: 500px;
+        width: 500px;
+        background-color: white;
+        border-radius: 10%;
+        color: var(--indigo-600);
+        justify-content: center;
+    }
+    .title{
+        margin-top: -7%;
+        margin-bottom: 5%;
+    }
+    .img-container{
+        
+        height: 300px;
+        width: 300px;
+    }
+    .img-container img{
+        width: 100%;
+        height: 100%;
+    }
+    .buttons{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        width: 350px;
+        
+    }
+    
+</style>
